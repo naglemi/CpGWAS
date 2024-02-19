d327a9b42a97419758b923ea77f42c3c6461c6bc
Michael Nagle <michael.nagle@oregonstate.edu>
2024-02-19 11:50:09 -0500

    debug, add tests, refactor (re)initialization of input

--- tests/testthat/test_loading_processing_inputs.R
+++ tests/testthat/test_loading_processing_inputs.R
@@ -79,6 +79,13 @@ test_that("Selected specific values in cov are as expected", {
 
 methylations <- regress_out_cov_parallel(methylations, cov)
 
+# test for identical results whether using regress_out_cov or regress_out_cov_parallel
+test_that("regress_out_cov and regress_out_cov_parallel produce identical results", {
+  adjusted_parallel <- regress_out_cov_parallel(methylations, cov)
+  adjusted_sequential <- regress_out_cov(methylations, cov)
+  expect_true(all.equal(adjusted_parallel, adjusted_sequential))
+})
+
 # 1. Test for Output Dimensions
 test_that("After regressing out covariates, methylations has correct dimensions", {
   expected_dimensions <- c(111, 11) # Replace with expected dimensions
@@ -101,5 +108,86 @@ test_that("After regressing out covariates, specific values in methylations are 
   tolerance <- 1e-6
   expect_true(abs(methylations["Br1003", 1] - 0.0427085754) < tolerance)
   expect_true(abs(methylations["Br1004", 2] - (-0.0200391506)) < tolerance)
+})
+
+pgen_path = system.file("extdata", "chr1_sample_subset.pgen", package = "CpGWAS")
+data(chr1_methylation_sample_subset, package = "CpGWAS")
+
+# Assuming MethylationInput has been properly defined and includes methods
+# for accessing processed methylations, genotype IDs, and covariates
+methInput <- new("MethylationInput", BSseq_obj = BSobj_sample, snp_data_path = pgen_path)
+
+# Assuming MethylationInput handles filtering based on genotype IDs internally
+test_that("Using `MethylationInput`, after filtering, methylation data has 111 samples", {
+  # This line mimics the filtering done inside MethylationInput; actual implementation may vary
+  filtered_methylations <- methInput@methylations[which(rownames(methInput@methylations) %in% methInput@genotype_IDs), ]
+  expect_equal(length(rownames(filtered_methylations)), 111)
+})
+
+test_that("Using `MethylationInput`, SNP data has 2189 samples as expected", {
+  expect_equal(length(methInput@psam$`#IID`), 2189)
+})
+
+test_that("Using `MethylationInput`, intersection of methylation, SNP data has 111 samples as expected", {
+  expect_equal(length(methInput@genotype_IDs), 111)
+})
+
+# Tests for covariate loading and processing using `MethylationInput`
+test_that("cov using `MethylationInput` has correct column names and types", {
+  expect_true(is.matrix(methInput@cov))
+  expected_column_names <- c("(Intercept)", "sexM", "primarydxSchizo", "agedeath", "pmi")
+  expect_equal(colnames(methInput@cov), expected_column_names)
+  expect_true(is.numeric(methInput@cov[, 2]))
+})
+
+test_that("cov using `MethylationInput` has the correct number of rows (samples)", {
+  expect_equal(nrow(methInput@cov), 111)
+})
+
+test_that("Selected specific values in cov using `MethylationInput` are as expected", {
+  expect_equal(methInput@cov["Br1003", "sexM"], 0)
+  expect_equal(methInput@cov["Br1004", "primarydxSchizo"], 0)
+  # Add more checks as needed for other specific values
+})
+
+test_that("After regressing out covariates using `MethylationInput`, methylations has correct dimensions", {
+  expected_dimensions <- c(111, 11) # Replace with expected dimensions
+  expect_equal(dim(methInput@methylations), expected_dimensions)
+})
+
+test_that("After regressing out covariates using `MethylationInput`, methylations contains numeric values", {
+  expect_true(is.numeric(methInput@methylations))
+})
+
+test_that("After regressing out covariates using `MethylationInput`, values in methylations are within expected range", {
+  expect_true(all(methInput@methylations >= -0.15 & methInput@methylations <= 0.11))
+})
+
+test_that("After regressing out covariates using `MethylationInput`, specific values in methylations are as expected", {
+  tolerance <- 1e-6
+  expect_true(abs(methInput@methylations["Br1003", 1] - 0.0427085754) < tolerance)
+  expect_true(abs(methInput@methylations["Br1004", 2] - (-0.0200391506)) < tolerance)
+})
+
+# Test for identical methylations and methInput@methylations
+test_that("methylations and methInput@methylations are identical", {
+  expect_true(all.equal(methylations, methInput@methylations))
+})
+
+test_that("Save and reload MethylationInput object maintains integrity", {
+  # Save MethylationInput object
+  saveRDS(methInput, file = "methInput_test.rds")
+  
+  # Reload MethylationInput object
+  reloadedMethInput <- reinitializeMethylationInput("methInput_test.rds", pgen_path)
+  
+  # Test: Compare original and reloaded objects
+  expect_equal(methInput@methylations, reloadedMethInput@methylations)
+  expect_equal(methInput@methylations_positions, reloadedMethInput@methylations_positions)
+  expect_equal(methInput@genotype_IDs, reloadedMethInput@genotype_IDs)
+  expect_equal(methInput@cov, reloadedMethInput@cov)
+  
+  # Cleanup
+  unlink("methInput_test.rds")
 })
 
